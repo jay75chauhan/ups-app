1abb4b38e26725597a83740c255cc0d430f84367
jay75chauhan <chauhan75jayk@gmail.com>
2022-09-18 12:35:03 +0530

    tw add

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //@ts-ignore - TailwindProvider is not typed
+    <TailwindProvider utilities={utilities}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#111",
     alignItems: "center",
     justifyContent: "center",
   },
